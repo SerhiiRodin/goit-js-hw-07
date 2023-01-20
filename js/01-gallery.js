a635a313153ab5f1f73c87e66a62741755a00360
SerhiiRodin <rodinserj@ukr.net>
2023-01-20 11:29:57 +0200

    add

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,8 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const inputEl = document.querySelector(".gallery");
+
+const result = _.add(2, 3);
+console.log(result); // 5
