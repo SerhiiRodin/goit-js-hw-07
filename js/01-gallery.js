1a59dc4105d2ad1a105ecd9ff11c24a9da3103b8
SerhiiRodin <rodinserj@ukr.net>
2023-01-22 16:42:16 +0200

    add task 1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,9 +1,67 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const galleryEl = document.querySelector(".gallery");
 
-const inputEl = document.querySelector(".gallery");
+const ListItemsMarkup = createListItemsMarkup(galleryItems);
 
-const result = _.add(2, 3);
-console.log(result); // 5
+function createListItemsMarkup(items) {
+  return items
+    .map(
+      (item) =>
+        `<div class="gallery__item">
+  <a class="gallery__link" href="${item.original}">
+    <img
+      class="gallery__image"
+      src="${item.preview}"
+      data-source="${item.original}"
+      alt="${item.description}"
+    />
+  </a>
+</div>`
+    )
+    .join("");
+}
+
+galleryEl.innerHTML = ListItemsMarkup;
+
+//----
+
+galleryEl.addEventListener("click", onGalleryClick);
+
+function onGalleryClick(event) {
+  event.preventDefault();
+
+  if (event.target.nodeName !== "IMG") {
+    return;
+  }
+
+  const urlImg = event.target.dataset.source;
+  const altImg = event.target.alt;
+
+  const modal = basicLightbox.create(
+    ` <img src="${urlImg}" alt="${altImg}"> `,
+    {
+      onShow: () => {
+        document.addEventListener("keydown", onKeyEscapeClick);
+      },
+      onClose: () => {
+        document.removeEventListener("keydown", onKeyEscapeClick);
+      },
+    }
+  );
+
+  function onKeyEscapeClick(event) {
+    event.stopPropagation();
+    // console.log(event.code);
+    if (event.code === "Escape") {
+      modal.close();
+    }
+    // return;
+  }
+
+  modal.show();
+
+  //   console.log(urlImg);
+  //   console.log(altImg);
+}
